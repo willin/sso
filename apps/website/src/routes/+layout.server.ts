b5ae9dc84dc2c8d830aec7f0dd6711b4730cb4d3
Willin Wang <i@v0.md>
2023-12-28 22:25:47 +0800

    ci(web): :bug: test bug

--- apps/website/src/routes/+layout.server.ts
+++ apps/website/src/routes/+layout.server.ts
@@ -1,9 +1,9 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch, request }) => {
-  const url = new URL(request.url);
-  const res = await fetch(`${url.origin}/auth/userinfo`, request);
-  const user = await res.json();
+  // const url = new URL(request.url);
+  const res = await fetch('/auth/userinfo', request);
+  const user = await res.json().catch(() => false);
   if (!user || user.error) {
     return {};
   }
