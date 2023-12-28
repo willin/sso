7ba5afb4250b3ed9d192d59bfb965130f34fd41b
Willin Wang <i@v0.md>
2023-12-28 22:22:13 +0800

    chore(web): :bug: test 500 bug

--- apps/website/src/routes/+layout.server.ts
+++ apps/website/src/routes/+layout.server.ts
@@ -1,7 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch, request }) => {
-  const res = await fetch('/auth/userinfo', request);
+  const url = new URL(request.url);
+  const res = await fetch(`${url.origin}/auth/userinfo`, request);
   const user = await res.json();
   if (!user || user.error) {
     return {};
